b82672bfe34a76dcf165f88e541cc26b7f0e74cb
Zaret Cardenas <dcarde10@itam.mx>
2023-11-26 14:28:40 -0600

    changes to predict from api and reflect in dash

--- API/main.py
+++ API/main.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
 from models import Todo
-
+import joblib
+from fastapi import UploadFile,File,HTTPException
+import os
+from tempfile import NamedTemporaryFile
+import pandas as pd
+import csv
+import codecs
+from io import StringIO
 app = FastAPI()
+
+
+obj = open("estimator_hyper_xgb.joblib","rb")
+model=joblib.load(obj)
+
+
+datadir="data/"
+
+import os
+dir="data"
+## If folder doesn't exists, create it ##
+if not os.path.isdir(dir):
+    os.mkdir(dir)
+
+        
 
 
 @app.get("/")
@@ -48,3 +70,43 @@ async def delete_todo(todo_id: int):
             todos.remove(todo)
             return{"message": "todo has been DELETED!"}
     return {"message": "No todos found"}
+
+
+
+@app.post("/check")
+def upload_file(file: UploadFile = File(...)):
+    temp = NamedTemporaryFile(delete=False)
+    try:
+        try:
+            contents = file.file.read()
+            with temp as f:
+                f.write(contents);
+            #with open(f{datadir}{file.filename}) as f:
+            #    f.write(contents);
+        except Exception:
+            raise HTTPException(status_code=500, detail='Error on uploading the file')
+        finally:
+            file.file.close()
+            
+        # Upload the file to your S3 service using `temp.name`
+        #s3_client.upload_file(temp.name, 'local', 'myfile.txt')
+        
+    except Exception:
+        raise HTTPException(status_code=500, detail='Something went wrong')
+    finally:
+        #temp.close()  # the `with` statement above takes care of closing the file
+        os.remove(temp.name)  # Delete temp file
+    
+    print(contents)  # Handle file contents as desired
+    s=str(contents,'utf-8')
+    data=StringIO(s)
+    df=pd.read_csv(data)
+    data.close()
+    #contents_df=pd.DataFrame(contents)
+    df[['pred1','pred2']] = model.predict_proba(df)
+
+    df.to_csv('data/test_pred.csv',index=True)
+
+    return {"filename": file.filename}
+
+#app.run_server(debug=True, port=8001)
